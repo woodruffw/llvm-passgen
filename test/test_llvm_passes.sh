297ad20fe85958693c22edc4c6e814c9bf5114d3
William Woodruff <william@trailofbits.com>
2019-09-26 19:21:44 -0400

    test: Fail early

--- test/test_llvm_passes.sh
+++ test/test_llvm_passes.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 LLVM_DIR=$(find /usr -name AddLLVM.cmake -printf '%h\n' -quit)
 [[ -d "${LLVM_DIR}" ]] || { >&2 echo "Couldn't infer LLVM_DIR"; exit 1; }
 export LLVM_DIR
