6483b0b47b9761a6e1f37d34e74465eb39e4976e
William Woodruff <william@yossarian.net>
2022-02-27 22:00:00 -0500

    src, test: remove BasicBlockPass (#74)
    * src, test: remove BasicBlockPass
    
    LLVM removed this a while ago.
    
    * test: avoid re-building just to run tests
    
    * test: pushd/popd

--- test/test_llvm_passes.sh
+++ test/test_llvm_passes.sh
@@ -6,12 +6,17 @@ LLVM_DIR=$(find /usr -name AddLLVM.cmake -printf '%h\n' -quit)
 [[ -d "${LLVM_DIR}" ]] || { >&2 echo "Couldn't infer LLVM_DIR"; exit 1; }
 export LLVM_DIR
 
-kinds=(module function block loop)
+llvm-passgen() {
+  cargo run --bin llvm-passgen -- "${@}"
+}
+
+kinds=(module function loop)
 
 for kind in "${kinds[@]}"; do
   dir=$(mktemp -d)
   llvm-passgen --dest "${dir}" --kind "${kind}" Test
-  cd "${dir}/Test/build" || { >&2 echo "Couldn't cd to pass build dir: ${dir}/build"; exit 1; }
+  pushd "${dir}/Test/build" || { >&2 echo "Couldn't chdir to pass build dir: ${dir}/build"; exit 1; }
   cmake ..
   make
+  popd
 done
