d41b669f7df072f827ef077a82d30bf8a65d8b47
William Woodruff <william@yossarian.net>
2022-04-04 15:50:12 -0400

    templates/common: compile all passes as C++17 (#81)
    * templates/common: compile as passes as C++17
    
    * workflows/ci: run tests on macOS
    
    * test: fix LLVM discovery on macOS

--- test/test_llvm_passes.sh
+++ test/test_llvm_passes.sh
@@ -2,7 +2,11 @@
 
 set -eo pipefail
 
-LLVM_DIR=$(find /usr -name AddLLVM.cmake -printf '%h\n' -quit)
+if [[ "$(uname)" = "Darwin" ]]; then
+  LLVM_DIR=$(find /usr/local/Cellar/llvm -name AddLLVM.cmake -print0 -quit | xargs -0 dirname)
+else
+  LLVM_DIR=$(find /usr -name AddLLVM.cmake -printf '%h\n' -quit)
+fi
 [[ -d "${LLVM_DIR}" ]] || { >&2 echo "Couldn't infer LLVM_DIR"; exit 1; }
 export LLVM_DIR
 
